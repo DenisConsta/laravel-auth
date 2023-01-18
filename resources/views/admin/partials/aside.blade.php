e4bce5c0d6914952467e880dcb4df691e47907b2
DenisConsta <denisconstantin338@gmail.com>
2023-01-18 17:03:04 +0100

    fix route aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,8 +1,16 @@
 <aside class="sidebar">
     <nav class="nav">
         <ul class="w-100">
-            <li class="active"><a href="#"><i class="fa-solid fa-diagram-project mx-3"></i>Projects</a></li>
-            <li><a href=" {{route('admin.projects.create')}} ">New Project</a></li>
+            <li class="
+            @if (request()->getpathInfo() == '/admin/projects')
+                active
+            @endif
+            "><a href="{{route('admin.projects.index')}}"><i class="fa-solid fa-diagram-project mx-3"></i> Project </a></li>
+            <li class="
+            @if (request()->getpathInfo() == '/admin/projects/create')
+                active
+            @endif
+            "><a href=" {{route('admin.projects.create')}} ">New Project</a></li>
 
         </ul>
     </nav>
